49051400a9fab6decb45ef52e37333e60615cfe9
realtrynna <realtrynna@gmail.com>
2023-01-20 11:28:48 +0900

    Docs: Reactivity, Computed, Binding, Rendering, Directive, Event, Watch 내용 정리

--- src/App.vue
+++ src/App.vue
@@ -1,73 +1,47 @@
 <template>
     <div>
-        <span>{{ reactiveMessage }}</span>
-        <button v-on:click="addReactiveMessage">Click</button>
-        <br />
-
-        <span>{{ normalMessage }}</span>
-        <button v-on:click="addNormalMessage">Click</button>
-
-        <span v-once>{{ value }}</span>
-
-        <button v-on:click="changeValue">테스트!!</button>
-
-        <br />
-
-        <input type="text" v-bind:value="title" />
-        <input v-bind="config" />
+        <button @click="change">Change!</button>
     </div>
 </template>
 
 <script lang="ts">
-import { ref, isRef, onUpdated, toRefs, toRef } from "vue";
+import { objectToString } from "@vue/shared";
+import {
+    ref,
+    isRef,
+    onUpdated,
+    toRefs,
+    toRef,
+    onMounted,
+    reactive,
+    computed,
+    watch,
+} from "vue";
 
 export default {
-    props: {
-        age: Number,
-    },
-    setup(props, { attrs, slots, emit, expose }) {
-        // const { age } = toRefs(props);
-        const age = toRef(props, "age");
-
-        const config = ref({
-            type: "password",
+    setup() {
+        const user = reactive({
+            name: "윤승근",
+            age: 30,
+            married: true,
         });
 
-        const value = ref("currentValue");
-        const title = ref("title");
+        const change = () => (user.married = false);
 
-        const reactiveMessage = ref("Reactive Message");
-        let normalMessage = "Normal Message";
-
-        const addReactiveMessage = () => {
-            console.log("실행");
-            reactiveMessage.value = "반응형 메세지입니다.";
-        };
-
-        const addNormalMessage = () => {
-            normalMessage = "일반 메세지입니다.";
-        };
-
-        const changeValue = () => {
-            value.value = "change Value!!!!!!!";
-        };
-
-        onUpdated(() => {
-            console.log("Updated Execute");
-        });
-
-        return {
-            reactiveMessage,
-            normalMessage,
-            value,
-            title,
-            config,
-            addReactiveMessage,
-            addNormalMessage,
-            changeValue,
-        };
+        watch(
+            () => user,
+            (newValue, oldValue) => {
+                console.log(newValue);
+            },
+            { deep: true }
+        );
+        return { user, change };
     },
 };
 </script>
 
-<style lang="scss" scoped></style>
+<style scoped>
+.active {
+    background-color: red;
+}
+</style>
